7a7cbfde37df9a2c8397074bc46bae15b05270fc
Tigran <41443306+septoon@users.noreply.github.com>
2024-09-12 13:40:53 +0300

    added navigate button

--- app/(tabs)/cart.jsx
+++ app/(tabs)/cart.jsx
@@ -4,8 +4,12 @@ import { useSelector, useDispatch } from 'react-redux';
 import { addDishToCart, decrementDishFromCart, removeDishFromCart, clearCart } from '../../redux/Features/cart/cartSlice';
 import CartItem from '../../components/CartItem';
 import tw from 'twrnc';
+import { useNavigation } from '@react-navigation/native';
+import { useHeaderHeight } from '@react-navigation/elements';
 
-const Cart = ({ navigation }) => {
+const Cart = () => {
+  const navigation = useNavigation();
+  const headerHeight = useHeaderHeight()
   const dispatch = useDispatch();
   const { items, totalCount, totalPrice } = useSelector(state => state.cart);
 
@@ -18,10 +22,24 @@ const Cart = ({ navigation }) => {
     }
   };
 
+  const handleClearCart = () => {
+    Alert.alert('Очистить корзину', 'Вы уверены, что хотите очистить корзину?', [
+      {
+        text: 'Отмена',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      {text: 'Да', onPress: () => dispatch(clearCart())},
+    ]);
+  };
+
   return (
-    <View style={{ flex: 1, padding: 20 }}>
+    <View style={[tw`w-full h-full border`, {paddingTop: headerHeight}]}>
       {items.length === 0 ? (
-        <Text>Вероятно, вы еще ничего не заказали. Переходите в меню для заказа.</Text>
+        <View >
+          <Text>Вероятно, вы еще ничего не заказали. Переходите в меню для заказа.</Text>
+          <Button title="Перейти в меню" onPress={() => navigation.navigate('index')} />
+        </View>
       ) : (
         <View style={tw`pt-29`}>
           {items.map((item, index) => (
@@ -39,9 +57,9 @@ const Cart = ({ navigation }) => {
       <Text>Всего товаров: {totalCount}</Text>
       <Text>Общая сумма: {totalPrice} ₽</Text>
 
-      <Button title="Очистить корзину" onPress={() => dispatch(clearCart())} />
+      <Button title="Очистить корзину" onPress={handleClearCart} />
       <Button title="Оформить заказ" onPress={handleCheckout} />
-      <Button title="Перейти в меню" onPress={() => navigation.navigate('/index')} />
+      <Button title="Перейти в меню" onPress={() => navigation.navigate('index')} />
     </View>
   );
 };
