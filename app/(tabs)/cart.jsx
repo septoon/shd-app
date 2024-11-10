faa98f6032562602a8a13c2176ec522032efafd2
Tigran <41443306+septoon@users.noreply.github.com>
2024-11-10 22:11:32 +0300

    modal from rn-modal

--- app/(tabs)/cart.jsx
+++ app/(tabs)/cart.jsx
@@ -1,6 +1,5 @@
-import { View } from 'react-native';
-import React from 'react';
-import { SafeAreaView, ScrollView, Modal } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, ScrollView, View, Text, Button } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import {
   addDishToCart,
@@ -12,44 +11,51 @@ import tw from 'twrnc';
 import EmptyCart from '../../components/Cart/EmptyCart';
 import FooterButtons from '../../components/Cart/FooterButtons';
 import { useColors } from '../../common/Colors';
+import Modal from 'react-native-modal';
 
 const Cart = () => {
   const dispatch = useDispatch();
-  const Colors = useColors()
+  const Colors = useColors();
   const { items, totalCount, totalPrice } = useSelector((state) => state.cart);
   const [modalVisible, setModalVisible] = useState(false);
 
   return (
-    <SafeAreaView style={tw`w-full h-full flex items-center justify-between pb-20 bg-[${Colors.darkModeBg}]`}>
+    <SafeAreaView style={[tw`w-full h-full flex items-center justify-between pb-20`, { backgroundColor: Colors.darkModeBg }]}>
       {items.length === 0 ? (
         <EmptyCart />
       ) : (
-          <ScrollView style={tw`w-full max-h-[100%] overflow-hidden rounded-xl`}>
-            {items.map((item, index) => (
-              <CartItem
-                key={index}
-                item={item}
-                onPlusDish={() => dispatch(addDishToCart(item))}
-                onMinusDish={() => dispatch(decrementDishFromCart(item))}
-                onRemoveDish={() => dispatch(removeDishFromCart(item))}
-              />
-            ))}
-            
-          </ScrollView>
+        <ScrollView style={tw`w-full max-h-[100%] overflow-hidden rounded-xl`}>
+          {items.map((item, index) => (
+            <CartItem
+              key={index}
+              item={item}
+              onPlusDish={() => dispatch(addDishToCart(item))}
+              onMinusDish={() => dispatch(decrementDishFromCart(item))}
+              onRemoveDish={() => dispatch(removeDishFromCart(item))}
+            />
+          ))}
+        </ScrollView>
       )}
-      <Modal visible={modalVisible}
-        animationType="slide"
-        transparent={true}
-        onRequestClose={() => setModalVisible(false)}>
-        <View style={tw`w-full h-full flex items-center justify-center`}>
-          <Text style={tw`text-lg text-[${Colors.darkModeText}]`}>Ваш заказ принят!</Text>
-        </View>
-      </Modal>
       <FooterButtons
         setModalVisible={setModalVisible}
         totalCount={totalCount}
         totalPrice={totalPrice}
       />
+      <Modal
+        isVisible={modalVisible}
+        onBackdropPress={() => setModalVisible(false)}
+        useNativeDriver={true}
+        style={tw`m-0`}
+      >
+        <View style={[tw`w-full h-full flex items-center justify-center`, { backgroundColor: Colors.darkModeBg }]}>
+          <Button
+            title="Закрыть"
+            onPress={() => setModalVisible(false)}
+            color={Colors.darkModeText}
+          />
+          <Text style={[tw`text-lg`, { color: Colors.darkModeText }]}>Ваш заказ принят!</Text>
+        </View>
+      </Modal>
     </SafeAreaView>
   );
 };
