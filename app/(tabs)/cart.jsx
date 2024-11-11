76db70c01baa40889f9dff1b7b91699bd43e01bf
Tigran <41443306+septoon@users.noreply.github.com>
2024-11-11 08:02:28 +0300

    without selected date

--- app/(tabs)/cart.jsx
+++ app/(tabs)/cart.jsx
@@ -10,7 +10,6 @@ import CartItem from '../../components/Cart/CartItem';
 import tw from 'twrnc';
 import EmptyCart from '../../components/Cart/EmptyCart';
 import OrderDialog from '../../components/Order/OrderDialog';
-import { formatDate, formatTime } from '../../common/formatDate';
 import FooterButtons from '../../components/Cart/FooterButtons';
 import { useColors } from '../../common/Colors';
 
@@ -19,11 +18,10 @@ const Cart = () => {
   const Colors = useColors()
   const { items, totalCount, totalPrice } = useSelector((state) => state.cart);
   const { orderType } = useSelector((state) => state.order);
-  const { selectedDate } = useSelector((state) => state.date);
   const [modalVisible, setModalVisible] = useState(false);
 
-  const shortDate = formatDate(selectedDate);
-  const shortTime = formatTime(selectedDate);
+  const shortDate = '11 нояб.';
+  const shortTime = '09:11';
 
   return (
     <SafeAreaView style={tw`w-full h-full flex items-center justify-between pb-20 bg-[${Colors.darkModeBg}]`}>
