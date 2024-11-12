7f2d6a517f2a20de8ed0518dcbfa96d3d87a7615
Tigran <41443306+septoon@users.noreply.github.com>
2024-11-12 11:53:05 +0300

    clear all elements from cart

--- app/(tabs)/cart.jsx
+++ app/(tabs)/cart.jsx
@@ -10,6 +10,7 @@ import CartItem from '../../components/Cart/CartItem';
 import tw from 'twrnc';
 import EmptyCart from '../../components/Cart/EmptyCart';
 import OrderDialog from '../../components/Order/OrderDialog';
+import { formatDate, formatTime } from '../../common/formatDate';
 import FooterButtons from '../../components/Cart/FooterButtons';
 import { useColors } from '../../common/Colors';
 
@@ -18,43 +19,16 @@ const Cart = () => {
   const Colors = useColors()
   const { items, totalCount, totalPrice } = useSelector((state) => state.cart);
   const { orderType } = useSelector((state) => state.order);
+  const { selectedDate } = useSelector((state) => state.date);
   const [modalVisible, setModalVisible] = useState(false);
 
-  const shortDate = '11 нояб.';
-  const shortTime = '09:11';
+  const shortDate = formatDate(selectedDate);
+  const shortTime = formatTime(selectedDate);
 
   return (
     <SafeAreaView style={tw`w-full h-full flex items-center justify-between pb-20 bg-[${Colors.darkModeBg}]`}>
-      {items.length === 0 ? (
-        <EmptyCart />
-        ) : (
-        <ScrollView style={tw`w-full max-h-[100%] overflow-hidden rounded-xl`}>
-          {items.map((item, index) => (
-            <CartItem
-              key={index}
-              item={item}
-              onPlusDish={() => dispatch(addDishToCart(item))}
-              onMinusDish={() => dispatch(decrementDishFromCart(item))}
-              onRemoveDish={() => dispatch(removeDishFromCart(item))}
-            />
-          ))}
-        </ScrollView>
-      )}
-      <OrderDialog
-        orderType={orderType}
-        modalVisible={modalVisible}
-        setModalVisible={setModalVisible}
-        items={items}
-        totalCount={totalCount}
-        totalPrice={totalPrice}
-        shortDate={shortDate}
-        shortTime={shortTime}
-      />
-      <FooterButtons
-        setModalVisible={setModalVisible}
-        totalCount={totalCount}
-        totalPrice={totalPrice}
-      />
+      <EmptyCart />
+      
     </SafeAreaView>
   );
 };
